59fc7910b9ad8918163d272b1a589afd8cff57c7
netwop <sasha50395@yandex.ru>
2026-02-07 23:25:14 +0300

    тестирование контента

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -1,20 +1,27 @@
 import pytest
 
 # Импортируем класс клиента.
+from django.contrib.auth import get_user_model
 from django.test.client import Client
+from django.conf import settings
+from django.urls import reverse
+from django.utils import timezone
 
 # Импортируем модель заметки, чтобы создать экземпляр.
-from news.models import News
+from news.models import News, Comment
 
+from datetime import datetime, timedelta
+
+User = get_user_model()
 
 @pytest.fixture
 # Используем встроенную фикстуру для модели пользователей django_user_model.
-def author(django_user_model):  
+def author(django_user_model):
     return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
-def not_author(django_user_model):  
+def not_author(django_user_model):
     return django_user_model.objects.create(username='Не автор')
 
 
@@ -34,17 +41,46 @@ def not_author_client(not_author):
 
 
 @pytest.fixture
-def new(author):
-    new = News.objects.create(  # Создаём объект заметки.
-        title='Заголовок',
-        text='Текст заметки',
-        author=author,
-    )
-    return new 
+def news(author):
+    today = datetime.today()
+    all_news = [
+        News(
+            title=f'Новость {index}',
+            text='Просто текст.',
+            # Для каждой новости уменьшаем дату на index дней от today,
+            # где index - счётчик цикла.
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    ]
+    News.objects.bulk_create(all_news)
+    return news
 
 @pytest.fixture
 def form_data():
     return {
         'title': 'Новый заголовок',
         'text': 'Новый текст',
-    } 
+    }
+
+@pytest.fixture
+def news_author_comments(author):
+    news = News.objects.create(
+        title='Тестовая новость', text='Просто текст.'
+    )
+    author = User.objects.create(username='Комментатор')
+    # Запоминаем текущее время:
+    now = timezone.now()
+    # Создаём комментарии в цикле.
+    comments = []
+    for index in range(10):
+        # Создаём объект и записываем его в переменную.
+        comment = Comment.objects.create(
+            news=news, author=author, text=f'Tекст {index}',
+        )
+        # Сразу после создания меняем время создания комментария.
+        comment.created = now + timedelta(days=index)
+        # И сохраняем эти изменения.
+        comment.save()
+        comments.append(comment)
+    return news, author, comments
--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -2,48 +2,67 @@
 import pytest
 from pytest_lazy_fixtures import lf
 
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.test import TestCase
+# Импортируем функцию reverse(), она понадобится для получения адреса страницы.
 from django.urls import reverse
-from notes.forms import NoteForm
+from datetime import datetime, timedelta
+
+from news.models import Comment, News
+from news.forms import CommentForm
 
 
-@pytest.mark.parametrize(
-    # Задаём названия для параметров:
-    'parametrized_client, note_in_list',
-    [
-        # Передаём фикстуры в параметры при помощи "ленивых фикстур":
-        (lf('author_client'), True),
-        (lf('not_author_client'), False),
-    ]
-)
-# Используем фикстуру заметки и параметры из декоратора:
-def test_notes_list_for_different_users(
-    note, parametrized_client, note_in_list
-):
-    url = reverse('notes:list')
-    # Выполняем запрос от имени параметризованного клиента:
-    response = parametrized_client.get(url)
+HOME_URL = reverse('news:home')
+
+
+def test_news_count(news, author_client):
+    # Загружаем главную страницу.
+    response = author_client.get(HOME_URL)
+    # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+    # Получаем список объектов из словаря контекста.
     object_list = response.context['object_list']
-    # Проверяем истинность утверждения "заметка есть в списке":
-    assert (note in object_list) is note_in_list
+    # Определяем количество записей в списке.
+    news_count = object_list.count()
+    # Проверяем, что на странице именно 10 новостей.
+    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
+def test_news_order(news, author_client):
+    response = author_client.get(HOME_URL)
+    object_list = response.context['object_list']
+    assert len(object_list) > 0
+    # Получаем даты новостей в том порядке, как они выведены на странице.
+    all_dates = [news.date for news in object_list]
+    # Сортируем полученный список по убыванию.
+    sorted_dates = sorted(all_dates, reverse=True)
+    # Проверяем, что исходный список был отсортирован правильно.
+    assert all_dates == sorted_dates
 
-@pytest.mark.parametrize(
-    # В качестве параметров передаём name и args для reverse.
-    'name, args',
-    (
-        # Для тестирования страницы создания заметки 
-        # никакие дополнительные аргументы для reverse() не нужны.
-        ('notes:add', None),
-        # Для тестирования страницы редактирования заметки нужен slug заметки.
-        ('notes:edit', lf('slug_for_args'))
-    )
-)
-def test_pages_contains_form(author_client, name, args):
-    # Формируем URL.
-    url = reverse(name, args=args)
-    # Запрашиваем нужную страницу:
-    response = author_client.get(url)
-    # Проверяем, есть ли объект формы в словаре контекста:
+def test_comments_order(news_author_comments, author_client):
+    news, author, comments = news_author_comments
+    response = author_client.get(reverse ('news:detail', args=(news.id,)))
+    # Проверяем, что объект новости находится в словаре контекста
+    # под ожидаемым именем - названием модели.
+    # Получаем объект новости.
+    news_from_response = response.context['news']
+    # Получаем все комментарии к новости.
+    all_comments = news_from_response.comment_set.all()
+    # Собираем временные метки всех комментариев.
+    all_timestamps = [comment.created for comment in all_comments]
+    # Сортируем временные метки, менять порядок сортировки не надо.
+    sorted_timestamps = sorted(all_timestamps)
+    # Проверяем, что временные метки отсортированы правильно.
+    assert all_timestamps == sorted_timestamps
+
+def test_anonymous_client_has_no_form(news_author_comments, client):
+    news, author, comments = news_author_comments
+    response = client.get(reverse ('news:detail', args=(news.id,)))
+    assert 'form' not in response.context
+
+def test_authorized_client_has_form(news_author_comments, author_client):
+    news, author, comments = news_author_comments
+    response = author_client.get(reverse ('news:detail', args=(news.id,)))
     assert 'form' in response.context
-    # Проверяем, что объект формы относится к нужному классу.
-    assert isinstance(response.context['form'], NoteForm)
+    # Проверим, что объект формы соответствует нужному классу формы.
+    assert isinstance (response.context['form'], CommentForm)
