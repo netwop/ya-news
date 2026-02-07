39d5bd01ba496fa6145b6b228e21f16ee1b684b2
Alexandra Shelukhina <sasha50395@yandex.ru>
2026-02-07 13:02:49 +0300

    pytest тестирование путей

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from news.models import News
+
+
+@pytest.fixture
+# Используем встроенную фикстуру для модели пользователей django_user_model.
+def author(django_user_model):  
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):  
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):  # Вызываем фикстуру автора.
+    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+    return client
+
+
+@pytest.fixture
+def new(author):
+    new = News.objects.create(  # Создаём объект заметки.
+        title='Заголовок',
+        text='Текст заметки',
+        author=author,
+    )
+    return new 
+
+@pytest.fixture
+def form_data():
+    return {
+        'title': 'Новый заголовок',
+        'text': 'Новый текст',
+    } 
---
+++ news/pytest_tests/test_routes.py
@@ -0,0 +1,57 @@
+import pytest
+from pytest_django.asserts import assertRedirects
+
+from pytest_lazy_fixtures import lf
+from http import HTTPStatus
+
+from django.urls import reverse
+
+from news.models import News, Comment
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:home', 'users:login', 'users:signup', 'users:logout', 'news:detail')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур 
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, new, expected_status
+):
+    url = reverse(name, args=(new.id,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+@pytest.mark.parametrize(
+    'name, args',
+    [
+        ('news:edit', lf('slug_for_args')),
+        ('news:delete', lf('slug_for_args')),
+    ],
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url) 
